21292e28c7ee87744c4a4f6ac1cb7e70822623c7
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-12 16:16:17 -0500

    add primary color to tailwinds config

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mt-10 flex flex-col">
+      <div className="flex flex-col gap-4 items-center">
+        <h1 className="text-4xl font-bold text-primary">Get the Best Gadgets at TechTrove!</h1>
+        <p className="text-xl text-gray-500">Explore the latest in technology and elevate your lifestyle with cutting-edge gadgets.</p>
+      </div>
     </div>
   );
 };
