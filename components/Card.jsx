4028b1b0cfe617c7b03311c0e68fd6231e06e0e3
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-12 21:27:28 -0500

    card layout basics done

--- components/Card.jsx
+++ components/Card.jsx
@@ -14,11 +14,11 @@ const Card = () => {
       <div className="relative my-10">
         <div>
           <h3 className="text-primary mb-3 font-semibold text-2xl">Macbook Pro M3 2023</h3>
-          <p className="text-gray-600">DESCRIPTION</p>
+          <p className="text-gray-600 text-xl">DESCRIPTION GOES HERE</p>
         </div>
       </div>
-      <span className="absolute bottom-0 right-0 text-primary font-semibold">
-          $15
+      <span className="absolute bottom-0 right-0 text-primary font-semibold bg-[#e8e5f0] p-2">
+          $300
       </span>
     </div>
   )
