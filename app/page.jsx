9f5f4c2976aebcbbefffca499a6ae5180a9ca29a
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-12 21:23:02 -0500

    add mostly finished card && landing page fixes

--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-10 flex flex-col max-w-7xl mx-auto">
+    <div className="mt-10 flex flex-col max-w-7xl mx-auto px-4">
       <div className="flex flex-col gap-4 items-center">
         <h1 className="text-4xl font-bold text-primary text-center">Get the Best Gadgets at TechTrove!</h1>
         <p className="text-xl text-gray-500 text-center">Explore the latest in technology and elevate your lifestyle with cutting-edge gadgets.</p>
       </div>
 
-      <div className="mt-10 space-y-4 px-4 py-8 grid grid-cols-1 lg:grid-cols-12 lg:py-16 lg:gap-8 xl:gap-0">
-          <div className="col-span-7">
-            <h2 className="text-4xl md:text-5xl lg:text-6xl font-extrabold mb-4 max-w-2xl">Discover Tech Wonders at GadgetGrove!</h2>
+      <div className="mt-10 space-y-4 flex justify-center items-center">
+          <div className="col-span-1 lg:col-span-7 max-w-screen-xl">
+            <h2 className="text-4xl md:text-5xl lg:text-6xl font-extrabold mb-6 lg:mb-8 max-w-2xl">Discover Tech Wonders at GadgetGrove!</h2>
             <p className="text-gray-500 font-light mb-6 lg:mb-8 lg:text-xl max-w-2xl">
               From innovative gadgets to cutting-edge tech, 
               GadgetGrove is your gateway to the future of technology. 
@@ -24,20 +24,25 @@ export default function Home() {
             </div>
 
           </div>
-          <div className="col-span-5">
+          <div className="hidden lg:block lg:col-span-5">
             <Image 
-              src="/logoplaceholder.webp"
+              src="/imageplaceholder.jpg"
               alt="cell phones"
-              height={400}
-              width={400}
+              height={450}
+              width={450}
             />
           </div>
         </div>
 
 
-        <div className="">
-          <h2 className="text-primary text-center font-bold text-4xl">Featured Products</h2>
-          <Card />
+        <div className="flex flex-col justify-center items-center">
+            <h2 className="text-primary text-center font-bold text-4xl my-10">Featured Products</h2>
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-16">
+              <Card />
+              <Card />
+              <Card />
+            </div>
           
         </div>
       
--- components/Card.jsx
+++ components/Card.jsx
@@ -2,22 +2,24 @@ import Image from "next/image";
 
 const Card = () => {
   return (
-    <div className="flex flex-col relative shadow-lg">
+    <div className="flex flex-col relative shadow-lg max-w-[400px]">
       <div>
         <Image 
-          src="/logoplaceholder.webp"
+          src="/imageplaceholder.jpg"
           alt="product image"
-          height={300}
-          width={300}
+          height={400}
+          width={400}
         />
       </div>
-      <div className="relative">
-        <h3>Macbook Pro M3 2023</h3>
-        <p>DESCRIPTION</p>
-        <span className="absolute top-0 right-0">
-          $15
-        </span>
+      <div className="relative my-10">
+        <div>
+          <h3 className="text-primary mb-3 font-semibold text-2xl">Macbook Pro M3 2023</h3>
+          <p className="text-gray-600">DESCRIPTION</p>
+        </div>
       </div>
+      <span className="absolute bottom-0 right-0 text-primary font-semibold">
+          $15
+      </span>
     </div>
   )
 }
